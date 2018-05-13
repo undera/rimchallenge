31b7bf318a6210f701fed458c94069dec915c677
Andrey Pohilko <apc4@ya.ru>
2018-05-13 23:21:52 +0300

    Changed into letter style

---
+++ Source/ChallengeAvailableLetter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Rimchallenge
+{
+	public class ChallengeAvailableLetter : StandardLetter
+	{
+		internal ChallengeDef challenge;
+        public ChallengeAvailableLetter()
+		{
+			def = LetterDefOf.PositiveEvent;
+		}
+
+		protected override IEnumerable<DiaOption> Choices
+		{
+			get
+			{
+				yield return Accept;
+				yield return base.Reject;
+			}
+		}
+
+		protected DiaOption Accept
+        {
+            get
+            {
+				return new DiaOption("Challenge Accepted!")
+                {
+                    action = delegate
+                    {
+						ChallengeManager.instance.StartChallenge(challenge);
+                        Find.LetterStack.RemoveLetter(this);
+                    },
+                    resolveTree = true
+                };
+            }
+        }
+	}
+}
--- Source/IncidentWorker_ChallengeAvailable.cs
+++ Source/IncidentWorker_ChallengeAvailable.cs
@@ -6,42 +6,43 @@ using Verse;
 
 namespace RimWorld
 {
-	public class IncidentWorker_ChallengeAvailable: IncidentWorker
-    {
+	public class IncidentWorker_ChallengeAvailable : IncidentWorker
+	{
 
-        protected override bool CanFireNowSub(IIncidentTarget target)
+		protected override bool CanFireNowSub(IIncidentTarget target)
 		{
 			return !ChallengeManager.instance.HasChallenge();
 		}
 
-        protected override bool TryExecuteWorker(IncidentParms parms)
-        {
+		protected override bool TryExecuteWorker(IncidentParms parms)
+		{
 			ChallengeDef offeredChallenge = ChallengeManager.instance.GetOfferedChallenge();
-            if (offeredChallenge == null)
-            {
-                Log.Message("No challenge to offer");
-                return false;
-            }
+			if (offeredChallenge == null)
+			{
+				Log.Message("No challenge to offer");
+				return false;
+			}
 
+			Find.LetterStack.ReceiveLetter(GetLetter(offeredChallenge), null);
+
+			return true;
+		}
+
+		private Letter GetLetter(ChallengeDef challenge)
+		{
 			string text = "Hey survivor!\n\nI'm interested in changing this world to better, and would appreciate your help by sharing some goods. Here's the task:\n";
-			text+="\n"+offeredChallenge.description;
-			text += "\n\n" + offeredChallenge.RewardsText();
-			text += "\n\nAre you capable of doing this?";
-            DiaNode dialog = new DiaNode(text);
-			DiaOption optAccepted = new DiaOption("Challenge Accepted!");
-            optAccepted.action = delegate
-            {
-				ChallengeManager.instance.StartChallenge(offeredChallenge);
-            };
-            optAccepted.resolveTree = true;
-            dialog.options.Add(optAccepted);
+			text += "\n" + challenge.description;
+			text += "\n\n" + challenge.RewardsText();
+            text += "\n\nAre you capable of doing this?";
 
-			DiaOption optReject = new DiaOption("Reject");
-			optReject.resolveTree = true;
-			dialog.options.Add(optReject);
-			string title = "Message Over Radio from a Stranger: "+offeredChallenge.LabelCap;
-            Find.WindowStack.Add(new Dialog_NodeTree(dialog, true, true, title));
-            return true;
-        }
-    }
+			ChallengeAvailableLetter letter = new ChallengeAvailableLetter();
+			letter.label = "Challenge Available";
+			letter.title = "Message Over Radio from a Stranger: " + challenge.LabelCap;
+			letter.text = text;
+			letter.radioMode = true;
+			letter.challenge = challenge;
+            
+			return letter;
+		}
+	}
 }
