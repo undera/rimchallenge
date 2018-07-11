78428a23e021de89d1f21a55291aa97319e0cd0b
Andrey Pohilko <apc4@ya.ru>
2018-07-11 14:37:00 +0300

    Base framework

---
+++ Source/Challenges.UI/Page_PickChallenge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Rimchallenge
+{
+	public class Page_PickChallenge : Page
+	{
+        public override string PageTitle
+		{
+			get
+			{
+				return "Pick Your Challenge";
+			}
+		}
+
+		public override void DoWindowContents(Rect inRect)
+		{
+			base.DrawPageTitle(inRect);
+			Rect mainRect = base.GetMainRect(inRect, 0f, false);
+			Widgets.ButtonText(mainRect, "Test2", true, false, true);
+			base.DoBottomButtons(inRect, null, null, null, true);
+		}
+	}
+}
---
+++ Source/Challenges.UI/ScenPart_PickChallenge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Rimchallenge
+{
+	public class ScenPart_PickChallenge:ScenPart_ConfigPage
+    {
+		public override void DoEditInterface(Verse.Listing_ScenEdit listing)
+		{
+			// this is called when user edits scenario
+			base.DoEditInterface(listing);
+		}
+    }
+}
